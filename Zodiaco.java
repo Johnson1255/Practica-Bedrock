b453b43f581df90f4eb27445457f9303a9682256
Johnson1255 <johnson.chen@upb.edu.co>
2022-04-05 18:03:14 -0500

    Codigo Base Zodiaco

---
+++ Zodiaco.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Zodiaco {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Dia de nacimiento (En Números)");
+        int dia = teclado.nextInt();
+
+        System.out.println("Mes de nacimiento (En Números)");
+        int mes = teclado.nextInt();
+    }
+    
+}
