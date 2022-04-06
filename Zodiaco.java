8cdb984d0581b6fcd15f9d901681224c51cbce51
Johnson1255 <johnson.chen@upb.edu.co>
2022-04-05 22:13:20 -0500

    Todavia Muchos Mas Zodiacos

--- Zodiaco.java
+++ Zodiaco.java
@@ -15,15 +15,15 @@ public class Zodiaco {
             switch (mes){
 
                 case 1:
-                if (dia > 0 && dia <= 20 && mes == 1){
-                    mesString = "Capricornio";
-                    break;
-                }
+                        if (dia > 0 && dia <= 20 && mes == 1){
+                         mesString = "Capricornio";
+                         break;
+                    }
 
-                else if(dia > 20 && dia <= 31 && mes == 1){
-                    mesString = "Acuario";
-                    break;
-                }
+                        else if(dia > 20 && dia <= 31 && mes == 1){
+                        mesString = "Acuario";
+                        break;
+                    }
 
                 case 2:
                         if(dia > 0 && dia <= 20 && mes == 2){
@@ -41,9 +41,52 @@ public class Zodiaco {
                             mesString = "Piscis";
                             break;
                     }
-                    
+
                         else if(dia > 20 && dia <= 31 && mes == 3){
                             mesString = "Aries";
+                            break;
+                    }
+
+                    case 4:
+                        if(dia > 0 && dia <= 20 && mes == 4){
+                            mesString = "Aries";
+                            break;
+                    }
+
+                        else if(dia > 20 && dia <= 30 && mes == 4){
+                            mesString = "Tauro";
+                            break;
+                    }
+                    case 5:
+                        if(dia > 0 && dia <= 20 && mes == 5){
+                            mesString = "Tauro";
+                            break;
+                    }
+
+                        else if(dia > 20 && dia <= 31 && mes == 5){
+                            mesString = "Géminis";
+                            break;
+                    }
+
+                    case 6:
+                        if(dia > 0 && dia <= 20 && mes == 6){
+                            mesString = "Géminis";
+                            break;
+                    }
+                    
+                        else if(dia > 20 && dia <= 30 && mes == 6){
+                            mesString = "Cáncer";
+                            break;
+                    }
+
+                    case 7:
+                        if(dia > 0 && dia <= 20 && mes == 7){
+                            mesString = "Cáncer";
+                            break;
+                    }
+
+                        else if(dia > 20 && dia <= 31 && mes == 7){
+                            mesString = "Leo";
                             break;
                     }
 
