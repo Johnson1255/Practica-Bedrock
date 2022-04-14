56642e5ce08829f68719b6fc5bec4a34ce8b0d1c
JuPaGonzalez <97990122+JuPaGonzalez@users.noreply.github.com>
2022-04-14 13:34:59 -0500

    Años en perro
    voy a ver si logro hacerla más precisa.

---
+++ añosperro.java
@@ -0,0 +1,20 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+
+public class añosperro {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Dime cuentos años tienes:");
+        int edad = teclado.nextInt();
+
+        System.out.println("Y cuantos meses llevas con esa edad?");
+        int mes = teclado.nextInt();
+
+        System.out.println("ahora calculemos tu edad en años de perro");
+        int edadperro = ((edad + ( mes / 12 )) / 7);
+        System.out.println(edadperro);}
+
+}
